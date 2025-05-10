b4d8c213af6c57f53f127339d0c43df4f14aa3cd
jogeswarnanda <jogeswarnanda@gmail.com>
2025-05-10 15:26:46 +0530

    jog!!

--- testjog/src/App.java
+++ testjog/src/App.java
@@ -2,7 +2,6 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hellouuuuuu, World888888@@!");
-        System.out.println("11111, World888888@@!");
+        System.out.println("JOgeswar, kan bhala ?");
     }
 }
