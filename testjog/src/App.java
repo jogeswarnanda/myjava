06bb28493715d8ddde2b1619a0ddfa5941d8afde
jogeswarnanda <jogeswarnanda@gmail.com>
2025-05-10 20:59:14 +0530

    today

--- testjog/src/App.java
+++ testjog/src/App.java
@@ -1,7 +1,21 @@
 
 public class App {
 
+    // My first java program
+    /* 
+     * Multi line comment
+     */
     public static void main(String[] args) {
         System.out.println("Baama , kan bhala ?");
+        System.out.println("I like pizaa ");
+
+        double age = 30.5;
+        int year = 2025;
+        System.out.println("The year is >> " + year);
+        double price = 19.99;
+        System.out.println("The price is  $" + price);
+        char str1 = 'S';
+        System.out.println("The char is  :" + str1);
+
     }
 }
