388e474cf1b0955960c965c1f8c76b07c9fb7288
jogeswarnanda <jogeswarnanda@gmail.com>
2025-05-10 15:24:17 +0530

    test jog

---
+++ testjog/src/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hellouuuuuu, World888888@@!");
+        System.out.println("11111, World888888@@!");
+    }
+}
